050a457476a1cd55ed513691e18d2c82e6d4744b
Martinvilla <villafanemartin78@gmail.com>
2025-11-07 17:35:22 -0300

    dejo para correr los templates (no oficial)

---
+++ frontend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/hola')
+def registration():
+    return render_template('hola.html')
+                           
+if __name__ == '__main__':
+    app.run(port= 5001 , debug=True)
